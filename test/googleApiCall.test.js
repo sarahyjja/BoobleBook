702f2eaaa568e7f0ce67618902f986482e37b56f
sarah <sarahkharraz@gmail.com>
2019-11-10 23:00:29 +0000

    Check if the response body returns valid infos

--- test/googleApiCall.test.js
+++ test/googleApiCall.test.js
@@ -10,6 +10,16 @@ describe("Google books API call integration", () => {
 
   // Test 2
   // Test what type of response we get for infos: title/authors/publisher
+  test("If the response body returns valid infos", done => {
+    makeGoogleApiCall("Pop").then(response => {
+      expect(typeof response.data.items[0].volumeInfo.title).toBe("string");
+      expect(Array.isArray(response.data.items[0].volumeInfo.authors)).toBe(
+        true
+      );
+      expect(typeof response.data.items[0].volumeInfo.publisher).toBe("string");
+    });
+    done();
+  });
 
   // Test 3
   // Error handling: test if passing an empty string will return a prompt 'Missing input'
