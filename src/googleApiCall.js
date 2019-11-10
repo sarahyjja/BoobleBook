d3bff39e41196d795a6f883d9a967040a561e5c1
sarah <sarahkharraz@gmail.com>
2019-11-10 22:20:09 +0000

    Dependencies + folder organisation: add Jest Sudo code for first tests + functions

---
+++ src/googleApiCall.js
@@ -0,0 +1,5 @@
+// Create the google book API call function
+
+// Mini function: getTitle
+// Mini function: getAuthor
+// Mini function: getPublisher
---
+++ test/googleApiCall.test.js
@@ -0,0 +1,8 @@
+// Test 1
+// Test if the function with a hardcoded word return a status code of 200
+
+// Test 2
+// Test what type of response we get for infos: title/authors/publisher
+
+// Test 3
+// Error handling: test if passing an empty string will return a prompt 'Missing input'
