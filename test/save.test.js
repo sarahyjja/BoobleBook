4f6e2aa34d69391ec399b6a436df927403668409
sarah <sarahkharraz@gmail.com>
2019-11-11 15:03:51 +0000

    Test if a list of objects is added

--- test/save.test.js
+++ test/save.test.js
@@ -1,15 +1,35 @@
 const fs = require("fs");
 const { save, read } = require("../src/save");
 
+afterEach(() => {
+  fs.unlinkSync("testStorage.json");
+});
+
 describe("Reading and saving some JSON", () => {
   test("If one object is added", () => {
     const data = { title: "Pop", authors: "Sarah", publisher: "Flammarion" };
-    const fileContent = read("testStorage.json");
-
     save("testStorage.json", data);
+    const fileContent = read("testStorage.json");
 
     expect(fileContent.title).toBe("Pop");
     expect(fileContent.authors).toBe("Sarah");
     expect(fileContent.publisher).toBe("Flammarion");
+  });
+
+  test("If a list of objects are added", () => {
+    const data = [
+      { title: "hello", authors: "Sarah", publisher: "Flammarion" },
+      { title: "Art", authors: "Anna", publisher: "Grasset" }
+    ];
+    save("testStorage.json", data);
+    const fileContent = read("testStorage.json");
+
+    expect(fileContent[0].title).toBe("hello");
+    expect(fileContent[0].authors).toBe("Sarah");
+    expect(fileContent[0].publisher).toBe("Flammarion");
+
+    expect(fileContent[1].title).toBe("Art");
+    expect(fileContent[1].authors).toBe("Anna");
+    expect(fileContent[1].publisher).toBe("Grasset");
   });
 });
