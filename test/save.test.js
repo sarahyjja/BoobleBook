2f37dd336474f95ecf9bb6300dcfc88e9e7b8433
sarah <sarahkharraz@gmail.com>
2019-11-11 14:50:24 +0000

    Test everything inside the object

--- test/save.test.js
+++ test/save.test.js
@@ -4,10 +4,12 @@ const { save, read } = require("../src/save");
 describe("Reading and saving some JSON", () => {
   test("If one object is added", () => {
     const data = { title: "Pop", authors: "Sarah", publisher: "Flammarion" };
-    save("testStorage.json", data);
-
     const fileContent = read("testStorage.json");
 
+    save("testStorage.json", data);
+
     expect(fileContent.title).toBe("Pop");
+    expect(fileContent.authors).toBe("Sarah");
+    expect(fileContent.publisher).toBe("Flammarion");
   });
 });
