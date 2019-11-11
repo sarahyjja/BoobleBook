bd4b2d19c8b9b57b7e16ce311f27b59596206d94
sarah <sarahkharraz@gmail.com>
2019-11-11 14:47:45 +0000

    Test if one object is added to a json file

--- src/save.js
+++ src/save.js
@@ -1,3 +1,13 @@
-// Create save function
+const fs = require("fs");
 
+// Create save function
+const save = (filename, content) => {
+  const stringifiedContent = JSON.stringify(content);
+  return fs.writeFileSync(filename, stringifiedContent);
+};
 //  Create read function
+const read = filename => {
+  const content = fs.readFileSync(filename, { encoding: "utf8" });
+  return JSON.parse(content);
+};
+module.exports = { save, read };
--- test/save.test.js
+++ test/save.test.js
@@ -1,1 +1,13 @@
+const fs = require("fs");
 const { save, read } = require("../src/save");
+
+describe("Reading and saving some JSON", () => {
+  test("If one object is added", () => {
+    const data = { title: "Pop", authors: "Sarah", publisher: "Flammarion" };
+    save("testStorage.json", data);
+
+    const fileContent = read("testStorage.json");
+
+    expect(fileContent.title).toBe("Pop");
+  });
+});
