10597746ad2f0ec73571562222a04a78adea6439
sarah <sarahkharraz@gmail.com>
2019-11-11 11:35:52 +0000

    Run app.js for frist tryout API call

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log("Here is app.js");
+const { makeGoogleApiCall } = require("./googleApiCall");
+const { searchBooks } = require("./commands");
+
+searchBooks(makeGoogleApiCall, "Pop").then(console.log("hello"));
