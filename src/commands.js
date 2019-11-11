f49690c222859cb3baf0aba02acd8a1c1161eb5f
sarah <sarahkharraz@gmail.com>
2019-11-11 01:20:56 +0000

    Check if the user get prompt for a query + mock request

--- src/commands.js
+++ src/commands.js
@@ -1,3 +1,17 @@
 // Create function searchBooks
+const searchBooks = (googleRequest, query) => {
+  return new Promise((resolve, reject) => {
+    googleRequest.getBooks(query).then(books => {
+      if (books.length === 0) {
+        resolve("Sorry! We haven't find any books matching your query.");
+      } else {
+        const message = "We found these books for you:";
+        resolve(message);
+      }
+    });
+  });
+};
 
 // Return the books in the console
+
+module.exports = { searchBooks };
--- test/commands.test.js
+++ test/commands.test.js
@@ -1,2 +1,27 @@
-// mock a request
 // get a CLI command from user
+const { searchBooks } = require("../src/commands");
+
+// mock a request
+const googleRequest = {
+  getBooks: query => {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          title: "fakeTitle",
+          authors: "fakeAuthors",
+          publisher: "fakePublisher"
+        }
+      ]);
+    });
+  }
+};
+
+describe("Create CLI command to search for books", () => {
+  // Prompt the user for a query to search a book
+  test("that the console prompt the user for a query to search a book", done => {
+    searchBooks(googleRequest, "query").then(message => {
+      expect(message).toBe("We found these books for you:");
+    });
+    done();
+  });
+});
